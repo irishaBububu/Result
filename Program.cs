c0b8d550828eb4e09821e52e7b4545c924e0364b
Irina <irisha-p@lst.ru>
2022-09-19 23:59:15 +0300

    Создан основной метод ArrayShortWords

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// В переданом массиве находим предлоги и слова с символами меньше 4 и
+// формируем новый масив
+string[] ArrayShortWords(string[] subs)
+{
+    string[] arSub = new string[subs.Length];
+    int i = 0;
+    foreach (string sub in subs)
+    {
+        if (sub.Length < 4)
+        {
+            arSub[i] = sub;
+            i++;
+        }
+    }
+    Array.Resize(ref arSub, i);
+    return arSub;
+}
